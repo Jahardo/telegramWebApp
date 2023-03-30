1f39c3dddd73fe524a63f18051ff191702410f24
Jahardo <dmorshch3@gmail.com>
2023-03-30 11:14:33 +0300

    new changes

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,48 @@
+import React, {useCallback, useEffect, useState} from 'react';
+import "./Form.css"
+import {useTelegram} from "../../hook/useTelegram";
+
+const Form = () => {
+    const [word, setWord] = useState("")
+    const [definition, setDefinition] = useState("")
+    const {tg} = useTelegram()
+
+    useEffect(() => {
+        tg.MainButton.setParams({
+            text: "Далі"
+        })
+    }, [])
+    const onSendData = useCallback(() => {
+        const data = {
+            word,
+            definition
+        }
+        tg.sendData(JSON.stringify(data))
+    }, [word, definition])
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData)
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
+    }, [onSendData])
+    useEffect(() => {
+        if (!definition || !word) {
+            tg.MainButton.hide()
+        } else {
+            tg.MainButton.show()
+        }
+    }, [word, definition])
+    return (
+        <div className={"form"}>
+            <h1>Заповніть данні</h1>
+            <label className={"label"}>Word</label>
+            <input placeholder={"write word"} type={"text"} className={"input"} value={word}
+                   onChange={e => setWord(e.target.value)}/>
+            <label className={"label"}>Definition</label>
+            <input placeholder={"write definition of wordnp"} type={"text"} className={"input"} value={definition}
+                   onChange={e => setDefinition(e.target.value)}/>
+        </div>
+    );
+};
+
+export default Form;
